eded6215bc420a39f41fb29250522a9e294a6a19
TahMoura <taliamoura0000@gmail.com>
2022-11-23 15:53:56 -0300

    TAREFA 2: Cactus and  Bird
    Obstacles Implementation:
    Class Small Cactus, Class Large Cactus and Class Bird implementation

---
+++ dino_runner/components/obstacles/bird.py
@@ -0,0 +1,21 @@
+from dino_runner.components.obstacles.obstacle import Obstacle
+from dino_runner.utils.constants import BIRD
+
+
+class Bird(Obstacle):
+    def __init__(self, image):
+        self.type = 0
+        self.image = image
+        super().__init__(image, self.type)
+        self.rect.y = 270
+        self.step_index = 0
+        
+
+    def draw(self, screen):
+        self.image = BIRD[0] if self.step_index < 5 else BIRD[1]
+        if self.step_index >= 10:
+            self.step_index = 0
+        screen.blit(self.image, self.rect)
+        self.step_index += 1
+
+        
